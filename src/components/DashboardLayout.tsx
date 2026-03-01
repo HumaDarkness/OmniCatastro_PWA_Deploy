b677d89f7879f061d3ac3e8c2506acf8983c9df0
Armando de Sousa Bustos <137858347+HumaDarkness@users.noreply.github.com>
2026-03-01 19:05:52 +0100

    fix: update DashboardLayout import to use DocumentalCentral to fix casing issues

--- src/components/DashboardLayout.tsx
+++ src/components/DashboardLayout.tsx
@@ -12,7 +12,7 @@ import {
     Building2,
     Users,
 } from "lucide-react";
-import { CentralDocumental } from "../CentralDocumental";
+import { CentralDocumental } from "../DocumentalCentral";
 import { ConsultaCatastral } from "../ConsultaCatastral";
 import { CalculadoraTermica } from "../CalculadoraTermica";
 import { ProyectosView } from "../ProyectosView";
